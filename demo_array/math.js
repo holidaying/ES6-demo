9bb1c7286b001d985223de15c18ae9837ed7c827
holidaying <workliudan@163.com>
2017-02-17 16:08:38 +0800

    es6函数操作， es6对象操作

--- demo_array/math.js
+++
@@ -1,5 +0,0 @@
-let array = [0,1,2,3,4];
-let arrNew = Array.from(array, x => x * x);
-console.log(arrNew);
-// 等同于
-let arrNew = Array.from(array).map(x => x * x);
--- demo_array/number.js
+++
@@ -1,9 +0,0 @@
-document.write("let array = [1, 4, -5, 10].find((n) => n < 0)"+'<br>');
-let array = [1, 4, -5, 10].find((n) => n < 0)
-document.write("array:", array+'<br>');
-document.write("let array = [1, 5, 10, 15].find(function(value, index, arr) {if(value>10) return index;}) "+'<br>');
-let arrayIndx = [1, 5, 10, 15].findIndex(function(value, index, arr) {
-	if(value>10)
-  	return index;
-}) 
-document.write("arrayIndex:"+arrayIndx);  // 10
